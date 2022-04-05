b91f92a79d451a93132c2fb32d890bab0b3c8ec1
Volodya Petrov <89377379140@mail.ru>
2022-04-06 01:00:32 +0300

    added multithreading loading files

--- Server/server.c
+++ Server/server.c
@@ -9,11 +9,18 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include "server.h"
 
 #define COUNT_OF_WAITING_CONNECTIONS  10
 #define FILE_NAME_SIZE 20
 #define FILE_PART_SIZE 512
+#define THREADS_COUNT 5
+
+pthread_mutex_t file_mutex;
+pthread_mutex_t accept_mutex;
+int listen_fd;
+char* directory_name;
 
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -25,7 +32,19 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int file_loading(int connection_fd, char* directory_name)
+int create_file(char* file_name, char* final_path)
+{
+    final_path = calloc(strlen(directory_name) + strlen(file_name) + 2, sizeof(char));
+    strcat(final_path, directory_name);
+    strcat(final_path, "/");
+    strcat(final_path, file_name);
+    pthread_mutex_lock(&file_mutex);
+    int fopen = open(final_path, O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    pthread_mutex_unlock(&file_mutex);
+    return fopen;
+}
+
+int file_loading(int connection_fd)
 {
     char buffer_for_name[FILE_NAME_SIZE + 1];
     char buffer_for_file[FILE_PART_SIZE];
@@ -39,11 +58,8 @@ int file_loading(int connection_fd, char* directory_name)
         return 1;
     }
     buffer_for_name[read_bytes] = '\0';
-    char* path = calloc(strlen(directory_name) + strlen(buffer_for_name) + 1, sizeof(char));
-    strcat(path, directory_name);
-    strcat(path, "/");
-    strcat(path, buffer_for_name);
-    int fopen = open(path, O_CREAT|O_EXCL|O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
+    char* path;
+    int fopen = create_file(buffer_for_name, path);
     if (fopen < 0)
     {
         perror("File create error");
@@ -134,21 +150,16 @@ int listen_on_port(const char* port)
     return socket_fd;
 }
 
-int start_server(const char* port, char* directory_name)
+void* work_with_client()
 {
-    char s[INET6_ADDRSTRLEN];
-    int socket_fd = listen_on_port(port);
-    if (mkdir(directory_name, S_IRWXU | S_IRWXG | S_IRWXO) != 0 && errno != EEXIST)
-    {
-        perror("Failed to create directory");
-        exit(1);
-    }
-    fprintf(stdout, "Server listening on port:%s", port);
-    while (1)
+    while(1)
     {
         struct sockaddr_storage connection_addr;
         socklen_t sin_size = sizeof connection_addr;
-        int connection_fd = accept(socket_fd, (struct sockaddr *)&connection_addr, &sin_size);
+        char s[INET6_ADDRSTRLEN];
+        pthread_mutex_lock(&accept_mutex);
+        int connection_fd = accept(listen_fd, (struct sockaddr *)&connection_addr, &sin_size);
+        pthread_mutex_unlock(&accept_mutex);
         if (connection_fd == -1)
         {
             perror("accept");
@@ -158,6 +169,30 @@ int start_server(const char* port, char* directory_name)
                   get_in_addr((struct sockaddr *)&connection_addr),
                   s, sizeof s);
 
-        file_loading(connection_fd, directory_name);
+        file_loading(connection_fd);
     }
+}
+
+int start_server(const char* port, char* directory)
+{
+    pthread_t threads[THREADS_COUNT];
+    directory_name = directory;
+    listen_fd = listen_on_port(port);
+    if (mkdir(directory_name, S_IRWXU | S_IRWXG | S_IRWXO) != 0 && errno != EEXIST)
+    {
+        perror("Failed to create directory");
+        exit(1);
+    }
+    pthread_mutex_init(&accept_mutex, NULL);
+    pthread_mutex_init(&file_mutex, NULL);
+    for (int i = 0; i < THREADS_COUNT; i++)
+    {
+        pthread_create(&threads[i], NULL, work_with_client, NULL);
+    }
+    for (int i = 0; i < THREADS_COUNT; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    close(listen_fd);
+    return 0;
 }
